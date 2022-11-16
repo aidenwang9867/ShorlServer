c43916db6154ac4d3b32947f019df14feacc017c
Yiding Wang <yidingwang9867@gmail.com>
2022-11-16 01:41:17 -0600

    post handler done

--- app/post_handlers.go
+++ app/post_handlers.go
@@ -1,14 +1,18 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"path"
+
+	"github.com/aidenwang9867/ShorlServer/utils"
 )
 
 func PostResultsHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := io.ReadAll(r.Body)
+	reqBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusBadRequest)
@@ -18,5 +22,26 @@ func PostResultsHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
+	bulkLinks := []string{}
+	err = json.Unmarshal(reqBody, &bulkLinks)
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		_, err := fmt.Fprint(w, "error unmarshaling inputs")
+		if err != nil {
+			log.Printf("error during Write: %v", err)
+		}
+		return
+	}
+	ret := []utils.Link{}
+	for _, l := range bulkLinks {
+		link := utils.Link{
+			Long:  l,
+			Short: path.Join("sho.rl/l", utils.EncodeLink(l)),
+		}
+		ret = append(ret, link)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(ret)
 }
